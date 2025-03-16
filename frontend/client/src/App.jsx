3702a60296e286cee253db9e74cceca3307d57ae
AEGfp <agusesgi@fpuna.edu.py>
2025-03-16 18:45:38 -0300

    Agregar barra de navegación

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { PermisosPage } from "./pages/PermisosPage";
 import { PermisosFormPage } from "./pages/PermisosFormPage";
+import { Navigation } from "./components/Navigation";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Navigate to="/" />}></Route>
         <Route path="/permisos" element={<PermisosPage />}></Route>
