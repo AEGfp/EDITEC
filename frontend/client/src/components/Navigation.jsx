2a711fb3e9a058bdeb6c03273c0c4b2112966b59
AEGfp <agusesgi@fpuna.edu.py>
2025-04-05 22:21:14 -0300

    Agregar Log out

--- frontend/client/src/components/Navigation.jsx
+++ frontend/client/src/components/Navigation.jsx
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function Navigation() {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+
+    navigate("/login");
+  };
+
   return (
     <div>
       <Link to="/">
@@ -9,6 +18,9 @@ export function Navigation() {
       <Link to="/permisos">Permisos</Link>
       <h1> </h1>
       <Link to="/permisos-crear">Crear permiso</Link>
+      <div>
+        <button onClick={handleLogout}>Cerrar sesión</button>
+      </div>
     </div>
   );
 }
