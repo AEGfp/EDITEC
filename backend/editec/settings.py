6761f050af3be65735a5bb4500f8607aec2fe23d
AEGfp <agusesgi@fpuna.edu.py>
2025-04-19 18:21:00 -0300

    Extender tiempo de tokens

--- backend/editec/settings.py
+++ backend/editec/settings.py
@@ -148,8 +148,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),  
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2), 
+'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),  
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1), 
     "BLACKLIST_AFTER_ROTATION": True,
     "ROTATE_REFRESH_TOKENS": True,
 }
