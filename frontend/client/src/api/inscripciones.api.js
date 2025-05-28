00e1cd66d2a7af4c5514c728f1d85cb73b6d0849
AEGfp <agusesgi@fpuna.edu.py>
2025-05-28 11:20:03 -0300

    Corregir API inscripciones

--- frontend/client/src/api/inscripciones.api.js
+++ frontend/client/src/api/inscripciones.api.js
@@ -2,10 +2,12 @@
 import { Api } from "./api";
 
 // Ruta relativa del recurso
-const DIRECCION = "educativo/inscripciones/";
+const DIRECCION = "inscripciones/";
 
 export const obtenerInscripciones = () => Api.get(DIRECCION);
-export const crearInscripcion = (inscripcion) => Api.post(DIRECCION, inscripcion);
+export const crearInscripcion = (inscripcion) =>
+  Api.post(DIRECCION, inscripcion);
 export const obtenerInscripcion = (id) => Api.get(`${DIRECCION}${id}/`);
-export const actualizarInscripcion = (id, datos) => Api.put(`${DIRECCION}${id}/`, datos);
+export const actualizarInscripcion = (id, datos) =>
+  Api.put(`${DIRECCION}${id}/`, datos);
 export const eliminarInscripcion = (id) => Api.delete(`${DIRECCION}${id}/`);
